3f4019f6ec6e6e6bcb80f3438c518c4ad59c8a6f
Alvin <alvin.2000@hotmail.co.uk>
2022-04-09 17:25:39 +0800

    Finish Splash Screen

---
+++ app/src/main/java/com/example/embeddedprogrammingassignment/SplashActivity.java
@@ -0,0 +1,49 @@
+package com.example.embeddedprogrammingassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.example.embeddedprogrammingassignment.user.LoginActivity;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        int SPLASH_SCREEN = 3500;
+
+        LottieAnimationView lottie = findViewById(R.id.lottieSplashLogo);
+
+        TextView appName = findViewById(R.id.tvAppName);
+        TextView appDesc = findViewById(R.id.tvAppDesc);
+        TextView developed = findViewById(R.id.tvDeveloped);
+
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.fade_splash);
+
+        lottie.setAnimation(animation);
+        appName.setAnimation(animation);
+        appDesc.setAnimation(animation);
+        developed.setAnimation(animation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN);
+    }
+}
