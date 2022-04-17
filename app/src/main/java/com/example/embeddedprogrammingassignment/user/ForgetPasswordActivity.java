409f2f2466f57a59e1da557db25aba7d261a26b4
Alvin <alvin.2000@hotmail.co.uk>
2022-04-17 19:57:55 +0800

    Forgot Password function

--- app/src/main/java/com/example/embeddedprogrammingassignment/user/ForgetPasswordActivity.java
+++ app/src/main/java/com/example/embeddedprogrammingassignment/user/ForgetPasswordActivity.java
@@ -9,10 +9,19 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.embeddedprogrammingassignment.MainActivity;
 import com.example.embeddedprogrammingassignment.R;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
 
 public class ForgetPasswordActivity extends AppCompatActivity {
 
@@ -28,23 +37,58 @@ public class ForgetPasswordActivity extends AppCompatActivity {
         resetPwd = findViewById(R.id.btnResetPwd);
         nricEt = findViewById(R.id.etForgetIC);
         phoneEt = findViewById(R.id.etForgetPhone);
-        pwdEt = findViewById(R.id.etForgetPhone);
+        pwdEt = findViewById(R.id.etForgetPwd);
         confirmedPwdEt = findViewById(R.id.etConfirmedPwd);
         toolbar = findViewById(R.id.toolbar);
 
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setTitle("Back");
 
         resetPwd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(ForgetPasswordActivity.this, MainActivity.class);
-                startActivity(intent);
+                resetPassword();
             }
         });
     }
+
+    private void resetPassword() {
+        String nric = nricEt.getEditText().getText().toString();
+        String phone = phoneEt.getEditText().getText().toString();
+        String pwd1 = pwdEt.getEditText().getText().toString();
+        String pwd2 = confirmedPwdEt.getEditText().getText().toString();
+
+        if(pwd1.equals(pwd2)) {
+            DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
+            Query findUser = reference.orderByChild("nric").equalTo(nric);
+
+            findUser.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if(snapshot.exists() && (Objects.equals(snapshot.child(nric).child("phone").getValue(String.class), phone))) {
+                        reference.child(nric).child("password").setValue(pwd1);
+                        Toast.makeText(getApplicationContext(), "Password reset success!", Toast.LENGTH_SHORT).show();
+
+                        Intent intent = new Intent(ForgetPasswordActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                    else
+                        Toast.makeText(getApplicationContext(), "Password reset failed! You have entered incorrect details.", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+        }
+        else
+            Toast.makeText(getApplicationContext(), "Your passwords does not match!", Toast.LENGTH_SHORT).show();
+    }
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
