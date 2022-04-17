b624ab6d1d0837138a83e9cd41a3d6e00d899f4a
Alvin <alvin.2000@hotmail.co.uk>
2022-04-17 15:39:30 +0800

    Implement Fragments

--- app/src/main/java/com/example/embeddedprogrammingassignment/MainActivity.java
+++ app/src/main/java/com/example/embeddedprogrammingassignment/MainActivity.java
@@ -2,17 +2,24 @@ package com.example.embeddedprogrammingassignment;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 
+import com.example.embeddedprogrammingassignment.fragments.HomeFragment;
+import com.example.embeddedprogrammingassignment.fragments.ProfileFragment;
+import com.example.embeddedprogrammingassignment.fragments.StatisticsFragment;
+import com.example.embeddedprogrammingassignment.fragments.TraceFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,34 +33,31 @@ public class MainActivity extends AppCompatActivity {
         //Initialize and Assign Value
         BottomNavigationView bottomNavigationView = findViewById(R.id.navbar);
 
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragContainer, new HomeFragment()).commit();
+
         //Set Home selected
         bottomNavigationView.setSelectedItemId(R.id.Home);
 
         //Perform ItemSelectedListener
-        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
-                    case R.id.Home:
-                        return true;
-
-                    case R.id.Statistic:
-                        startActivity(new Intent(getApplicationContext(), StatisticsActivity.class));
-                        overridePendingTransition(0,0);
-                        return true;
-
-                    case R.id.Trace:
-                        startActivity(new Intent(getApplicationContext(), TraceActivity.class));
-                        overridePendingTransition(0,0);
-                        return true;
-
-                    case R.id.Profile:
-                        startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                }
-                return false;
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment fragment = null;
+            switch (item.getItemId()) {
+                case R.id.Home:
+                    fragment = new HomeFragment();
+                    break;
+                case R.id.Statistic:
+                    fragment = new StatisticsFragment();
+                    break;
+                case R.id.Trace:
+                    fragment = new TraceFragment();
+                    break;
+                case R.id.Profile:
+                    fragment = new ProfileFragment();
+                    break;
             }
+            assert fragment != null;
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragContainer, fragment).commit();
+            return true;
         });
     }
 }
