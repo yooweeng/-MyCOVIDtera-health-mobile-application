7dbe8b6505b0489727dab5347d03b26a4e141d67
SWE1904867@xmu.edu.my <SWE1904867@xmu.edu.my>
2022-07-05 21:01:49 +0800

    Implement checkout function

--- app/src/main/java/com/example/embeddedprogrammingassignment/fragments/trace/QrHistoryFragment.java
+++ app/src/main/java/com/example/embeddedprogrammingassignment/fragments/trace/QrHistoryFragment.java
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.embeddedprogrammingassignment.R;
 import com.example.embeddedprogrammingassignment.adapter.QrHistoryAdapter;
